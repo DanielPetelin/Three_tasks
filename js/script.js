05f6a04e81f56366bf5282e90d1d606e5452ab07
DanielPetelin <daniilpetelin77@gmail.com>
2020-03-23 14:47:47 +0300

    add first less

--- js/script.js
+++ js/script.js
@@ -12,6 +12,9 @@ document.getElementById('push').onclick = function() {
     var yourNum = document.getElementById('numbers').value;
     var yourAge = document.getElementById('yourAge').value;
     var cost = document.getElementsByName('cost');
+    // if (yourAge == '') {
+    //     alert('заполните поле возраста!');
+    // };
     for (var i = 0; i < cost.length; i++) {
         if (cost[i].type == "radio" && cost[i].checked) {
             console.log('Your num: ' + yourNum + ';   Cost: ' + cost[i].value + ';   Your age: ' + yourAge);
@@ -19,3 +22,19 @@ document.getElementById('push').onclick = function() {
     };
     // console.log();
 };
+
+// ==========================
+// задача 3
+document.getElementById('bgColor').onclick = function() {
+    var bgColor = document.querySelector('.bgColor').value;
+    var page = document.querySelector('body');
+
+    page.style.backgroundColor = bgColor;
+};
+
+// document.getElementById('bgColor').onclick = function() {
+//     var bgColor = document.getElementById('bgColor').value;
+//     var page = document.querySelector('body');
+
+//     page.style.backgroundColor = bgColor;
+// };
