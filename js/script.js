bebbce0db6c1e1f8e93bcf656dd16689b98cc2a5
DanielPetelin <daniilpetelin77@gmail.com>
2020-03-23 13:00:34 +0300

    add 2

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ var yearNow = 2020;
 var yourYears = yearNow - yearAgo;
 
 document.getElementById('out').innerHTML = 'Ваш возраст: ' + yourYears
+
+// задача два
+document.getElementById('push').onclick = function() {
+    var yourNum = document.getElementById('numbers').value;
+    var cost = document.getElementsByClassName('cost').value;
+    var yourAge = document.getElementById('yourAge').value;
+
+    console.log(yourNum + ' ' + cost + ' ' + yourAge);
+    // console.log();
+};
